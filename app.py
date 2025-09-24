94d01d47ccac5e6e18b53bd2013f072bfed9e4b3
Nikhil-tej108 <nikhiltej99@gmail.com>
2025-09-24 10:40:34 +0530

    commit

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Bazinga!</h1><p>Hello from Sheldon’s first Flask app on Azure.</p>"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
